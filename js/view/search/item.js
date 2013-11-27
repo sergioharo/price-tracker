b6887769305b65415aed91151dc59bd2b3e22968
sergioharo <sergio.haro.jr@gmail.com>
2013-11-27 10:09:42 -0500

    creating search view that displays matching products

---
+++ js/view/search/item.js
@@ -0,0 +1,23 @@
+define(["parse", "underscore"], function (Parse, _) {
+	return Parse.View.extend({
+		template: _.template($("#search-result-template").html()),
+
+		tagName: "li",
+
+		className: "search-result",
+
+		events: {
+
+		},
+
+		initialize: function () {
+
+		},
+
+		render: function () {
+			this.$el.data("model", this.model);
+			this.$el.html(this.template(this.model.toJSON()));
+			return this;
+		}
+	});
+});
